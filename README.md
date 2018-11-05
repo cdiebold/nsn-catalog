e22f59f1060db8be65991d6e029f40e10c0924f3
Chris Diebold <cdiebold2012@gmail.com>
2018-11-05 06:45:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Description
+The NSN catalog allows a user to search for NSN parts and submit a quote for a part.
+
+# Directory Structure
+
+
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+// [Style] src and desitnation
+const sassSrc = 'static/scss';
+const sassDest = 'static/css';
+
+// [IMG] Image processing
+gulp.task('sass', function() {
+
+});
+
